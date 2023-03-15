d0ab0e616895f38df0267f566b97f0c97cd0b842
MrHeadShok <tbelkahla3@gmail.com>
2023-03-15 23:45:47 +0100

    ThreeJ Fiber fixed & drei - hero ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ const Hero = () => {
             gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-3 h-3 rounded-full bg-[#ff5e71]" />
-          <div className="w-1 sm:h-80 h-40 w-0.5 bg-[#ff5e71]" />
+          <div className="w-3 h-3 rounded-full bg-[#51f951]" />
+          <div className="w-1 sm:h-80 h-40 w-0.5 bg-[#51f951]" />
         </div>
 
         <div>
@@ -20,7 +20,7 @@ const Hero = () => {
             className={`${styles.heroHeadText}
                     text-white`}
           >
-            Hey, I'm <span className="text-[#915eff]"> Taieb 👋🏻</span>
+            Hey, I'm <span className="text-[#51f951]"> Taieb 👋🏻</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop web applications, mobile applications{" "}
@@ -28,7 +28,28 @@ const Hero = () => {
           </p>
         </div>
       </div>
-        <ComputersCanvas />
+      <ComputersCanvas />
+      <div
+        className="absolute xs:bottom-10 bottom-32
+    w-full flex justify-center items-center"
+      >
+        <a href="#about">
+          <div
+            className="w-[35px] h-[64px] rounded-3xl border-4 border-[#51f95177]
+        flex justify-center items-start p-2"
+          >
+<motion.div 
+animate={{y:[0, 24, 0]}}
+transition={{duration:1.5,
+repeat: Infinity,
+repeatType:'loop'
+}}
+className="w-3 h-3 rounded-full bg-[#51f951e2] mb-1"
+/>
+
+          </div>
+        </a>
+      </div>
     </section>
   );
 };

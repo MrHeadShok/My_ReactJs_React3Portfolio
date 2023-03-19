8a4bda0b97b6d1454edf0dcc869d40db88d2e331
MrHeadShok <tbelkahla3@gmail.com>
2023-03-19 22:19:00 +0100

    Experience & tech okay § mesh added correctly

--- src/constants/index.js
+++ src/constants/index.js
@@ -45,7 +45,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Ionic framework Developer",
       icon: mobile,
     },
     {
@@ -53,7 +53,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Fitness enthusiast",
       icon: creator,
     },
   ];
@@ -115,38 +115,35 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
+      title: "Full stack Developer",
+      company_name: "Pixelraise",
       icon: pixelraise,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      iconBg: "#E6DEDD",
+      date: "May 2019 - September 2019",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Working on developping additional plug-ins & features in existing web apps.",
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
+      title: "AI trainer",
+      company_name: "Hoist Finance",
       icon: hoistfinance,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Jul 2021 - Feb 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developping and maintaining conversational AI chatbot.",
+      "Contribute in API review & implementation.",  
       ],
     },
 
     {
-      title: "Full stack Developer",
-      company_name: "Pixelraise",
+      title: "Channel & digital product owner",
+      company_name: "Hoist Finance",
       icon: hoistfinance,
       iconBg: "#E6DEDD",
-      date: "May 2019 - September 2019",
+      date: "Feb 2022 - Present",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
